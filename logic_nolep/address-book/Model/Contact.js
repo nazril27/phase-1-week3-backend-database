8f375198e77118330cdf66b6890652e62606318d
nazril <dnazrilsanz@gmail.com>
2026-06-10 18:36:37 +0700

    fix sedikit

--- logic_nolep/address-book/Model/Contact.js
+++ logic_nolep/address-book/Model/Contact.js
@@ -1,23 +1,15 @@
 const db = require('./../connection');
 
 class Contact {
-    constructor(name, phoneNumber, company, email) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.company = company;
-        this.email = email;
-    }
-
     static create(name, phoneNumber, company, email) {
-        let newContact = new Contact(name, phoneNumber, company, email);
-        const arr = [newContact.name, newContact.phoneNumber, newContact.company, newContact.email];
+        const arr = [name, phoneNumber, company, email];
         return new Promise((resolve, reject) => {
             db.run(`INSERT INTO Contact VALUES (null, ?, ?, ?, ?)`, 
                 arr, (err) => {
                     if (err) {
                         reject(err.message);
                     } else {
-                        resolve(`Kontak baru ${newContact.name} berhasil ditambahkan.`);
+                        resolve(`Kontak baru ${name} berhasil ditambahkan.`);
                     }
                 });
             }
